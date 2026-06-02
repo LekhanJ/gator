85ccc312af805672ebe76a33903b909c98f95c76
LekhanJ <jomiwalelekhan@gmail.com>
2026-06-02 15:08:58 +0530

    Add database operations

--- src/commander.ts
+++ src/commander.ts
@@ -1,10 +1,11 @@
-import { setUser } from "./config.js";
+import { readConfig, setUser } from "./config.js";
+import { createUser, getUser, getUsers } from "./lib/db/queries/users.js";
 import { assertExists } from "./utils.js";
 
-export type CommandHandler = (cmdName: string, ...args: string[]) => void;
+export type CommandHandler = (cmdName: string, ...args: string[]) => Promise<void>;
 export type CommandsRegistry = Record<string, CommandHandler>;
 
-export function handlerLogin(cmdName: string, ...args: string[]): void {
+export async function handlerLogin(cmdName: string, ...args: string[]): Promise<void> {
     if (args.length !== 1) {
         throw new Error("The login command expects a single argument, the username");
     }
@@ -13,21 +14,63 @@ export function handlerLogin(cmdName: string, ...args: string[]): void {
 
     assertExists(username, "Username is missing");
 
+    const result = await getUser(username);
+    if (!result) {
+        throw new Error("User does not exists. Please register before logging in!");
+    }
+
     setUser(username);
 
-    console.log("User has been set");
+    console.log("Login successful. User has been set");
     
+}
+
+export async function handlerRegister(cmdName: string, ...args: string[]): Promise<void> {
+    if (args.length !== 1) {
+        throw new Error("The register command expects a single argument, the username");
+    }
+
+    const username = args[0];
+
+    assertExists(username, "Username is missing");
+
+    const result = await getUser(username);
+    if (result) {
+        throw new Error("User already exists. Please login");
+    }
+
+    const createdUser = await createUser(username);
+
+    setUser(username);
+
+    console.log(
+        `User created: ${JSON.stringify(createdUser)}`
+    );
+}
+
+export async function handlerUsers(cmdName: string): Promise<void> {
+    const users = await getUsers();
+    const config = readConfig();
+    users.forEach((user) => {
+        if (user.name === config.currentUserName) {
+            console.log(`${user.name} (current)`);
+        } else {
+            console.log(user.name);
+        }
+    })
 }
 
 export function registerCommand(registry: CommandsRegistry, cmdName: string, handler: CommandHandler): void {
     registry[cmdName] = handler;
 }
 
-export function runCommand(registry: CommandsRegistry, cmdName: string, ...args: string[]): void {
-    if (!registry[cmdName]) {
-        throw new Error("Command does not exist!");
-    }
+export async function runCommand(registry: CommandsRegistry, cmdName: string, ...args: string[]): Promise<void> {
     const handler = registry[cmdName];
 
-    handler(cmdName, ...args);
+    assertExists(
+        handler,
+        `Command ${cmdName} does not exist`
+    );
+
+    await handler(cmdName, ...args);
 }
---
+++ src/main.ts
@@ -0,0 +1,39 @@
+import { handlerLogin, handlerRegister, handlerUsers, registerCommand, runCommand, type CommandsRegistry } from "./commander.js"
+import { argv } from 'node:process';
+import { assertExists } from "./utils.js";
+
+const registry: CommandsRegistry = {};
+
+registerCommand(registry, "login", handlerLogin);
+registerCommand(registry, "register", handlerRegister);
+registerCommand(registry, "users", handlerUsers);
+
+async function main() {
+  const args = argv.slice(2);
+
+  if (args.length < 1) {
+    console.error("Not enough arguments were provided");
+    process.exit(1);
+  }
+
+  const cmdName = args[0];
+  const cmdArgs = args.slice(1);
+
+  assertExists(cmdName);
+
+  try {
+    await runCommand(registry, cmdName, ...cmdArgs);
+  } catch (err) {
+      if (err instanceof Error) {
+          console.error(err.message);
+      } else {
+          console.error(err);
+      }
+
+      process.exit(1);
+  }
+
+  process.exit(0)
+}
+
+main()
